c710be3de7d054857a24e2bc0d22765716607dc0
Saranya Mandapaty <saranya.broadway@gmail.com>
2023-06-23 14:10:24 -0700

    Initial Commit

---
+++ all about me/all_about_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  all_about_meApp.swift
+//  all about me
+//
+//  Created by Saranya Mandapaty on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct all_about_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ all about meUITests/all_about_meUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  all_about_meUITestsLaunchTests.swift
+//  all about meUITests
+//
+//  Created by Saranya Mandapaty on 6/23/23.
+//
+
+import XCTest
+
+final class all_about_meUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
