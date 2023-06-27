08ba0fc03d809e627c0519a56fe386e66984f1ea
Saranya Mandapaty <saranya.broadway@gmail.com>
2023-06-27 09:23:48 -0700

    i want to commit

--- all about me/ContentView.swift
+++ all about me/ContentView.swift
@@ -8,19 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    @State var name = ""
+       @State var factsRevealed = false
+       var textTitle = "What is your name? "
+       var imageName = "newspaper"
+       var facts = [
+           "I love going on bike rides.",
+           "Dream is to work at the NYT",
+           "I love reading international classics.",
+           "I love art but cannot paint, my favorite medium is colored pencil and graphite.",
+           "I want to visit Greece (inspired by Mamma Mia)."
+       ]
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+       var body: some View {
+           VStack {
+               Text("Welcome, Saranya Mandapaty!")
+                   .font(.title)
+                   .fontWeight(.bold)
+    
+
+               Image(imageName)
+                   .resizable()
+                   .aspectRatio(contentMode: .fit)
+                   .frame(width: 200, height: 200)
+                   .padding()
+
+               Button("Show facts") {
+                   factsRevealed.toggle()
+               }
+               .foregroundColor(.white)
+               .padding()
+               .background(Color.purple)
+               .cornerRadius(10)
+
+               if factsRevealed {
+                   ForEach(facts, id: \.self) { fact in
+                       Text(fact)
+                           .font(.subheadline)
+                           .fontWeight(.bold)
+                           .foregroundColor(.purple)
+                           .multilineTextAlignment(.leading)
+                           .padding()
+                   }
+               }
+           }
+           .padding()
+       }
+   }
